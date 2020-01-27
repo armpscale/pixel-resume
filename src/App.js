6b06ee030e03ec09fe023db0ee0a3362fdf9ba89
armpscale <akkrawut33588@gmail.com>
2020-01-28 01:38:26 +0700

    Character : Add character for test spritesheet with module

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import CanvasChart from './components/fieldBackground';
+import Character from './components/character';
 
 class App extends React.Component {
 
@@ -13,21 +12,7 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <CanvasChart />
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Character />
       </div>
     );
   }
---
+++ src/components/character/index.jsx
@@ -0,0 +1,87 @@
+import React from 'react';
+import Spritesheet from 'react-responsive-spritesheet';
+import './style.css';
+import image from '../../spritesheet/character/ForteSpriteSheet.png'
+
+class Character extends React.Component {
+    constructor() {
+        super();
+        this.escFunction = this.escFunction.bind(this);
+    }
+    componentDidMount() {
+        document.addEventListener("keydown", this.escFunction, false);
+    }
+    componentWillUnmount() {
+        document.removeEventListener("keydown", this.escFunction, false);
+    }
+
+    escFunction(event) {
+        console.log(event.keyCode)
+        if (event.keyCode === 27) {
+            this.spritesheeInstance.play();
+        }
+    }
+
+    myFunctionPlay() {
+        this.spritesheeInstance.play();
+    }
+
+    myFunctionPause() {
+        this.spritesheeInstance.pause();
+    }
+
+    myFunctionGetFrame() {
+        alert(this.spritesheeInstance.getInfo('frame'));
+    }
+
+    myFunctionToggleDirection() {
+        if (this.spritesheeInstance.getInfo('direction') === 'forward') {
+            this.spritesheeInstance.setDirection('rewind');
+        } else if (this.spritesheeInstance.getInfo('direction') === 'rewind') {
+            this.spritesheeInstance.setDirection('forward');
+        }
+    }
+
+    render() {
+        return (
+            <div onKeyDown={this._handleKeyDown}>
+                <Spritesheet
+                    className={`my-element__class--style`}
+                    // image={`https://raw.githubusercontent.com/danilosetra/react-responsive-spritesheet/master/assets/images/examples/sprite-image-horizontal.png`}
+                    image={image}
+                    widthFrame={70}
+                    heightFrame={140}
+                    steps={3}
+                    fps={10}
+                    // timeout={1000}
+                    autoplay={true}
+                    loop={true}
+                    isResponsive={true}
+                    background={`https://raw.githubusercontent.com/danilosetra/react-responsive-spritesheet/master/assets/images/examples/sprite-image-background.png`}
+                    backgroundSize={`cover`}
+                    backgroundRepeat={`no-repeat`}
+                    backgroundPosition={`center center`}
+                    getInstance={spritesheet => {
+                        this.spritesheeInstance = spritesheet;
+                    }}
+                    onEachFrame={spritesheet => {
+                        if (spritesheet.getInfo('frame') == 1){
+                            spritesheet.pause()
+                            setTimeout(() => {
+                                spritesheet.play()
+                              }, 3000);
+                        }
+                      }}
+                />
+                <div>
+                    <button onClick={this.myFunctionPlay.bind(this)}>play</button>
+                    <button onClick={this.myFunctionPause.bind(this)}>pause</button>
+                    <button onClick={this.myFunctionGetFrame.bind(this)}>alert current frame</button>
+                    <button onClick={this.myFunctionToggleDirection.bind(this)}>toggle direction</button>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Character;
